dc204872872414ef83e58659654a2397d3c73c37
NellySaavedra <oficinadenellys.sistemas@gmail.com>
2016-04-29 13:45:04 -0500

    Anexo de diagramas

--- Ejercicio 10 - Tienda de Autoservicio/Problema/main.js
+++ Ejercicio 10 - Tienda de Autoservicio/Problema/main.js
@@ -1,1 +1,13 @@
 //Ejercicio #10: Tienda de autoservicio
+
+var Ciudades = prompt("¿Cuántas ciudades vamos a contemplar?")
+var Tiendas = prompt("¿Cuántas tiendas tiene cada Ciudad?")
+var Empleados = prompt("¿Cuántos empleados tiene cada tienda?")
+var VxE = prompt("Cuánto vendió cada empleado por día?")
+var VxT = 0 //Venta por tienda
+var VxC = 0 //Venta por ciudad
+var VxD = 0 //Venta por día de toda la cadena
+
+
+
+
--- Ejercicio 6 - Mayores a 0/Problema/main.js
+++ Ejercicio 6 - Mayores a 0/Problema/main.js
@@ -1,1 +1,7 @@
-//Escribe aquí tú código
+//Ejercicio #6: Mayores a 0
+
+// Se requiere un algoritmo para determinar, de N cantidades, cuántas son menores o iguales a cero y cuántas mayores a cero.
+
+// Realice el diagrama de flujo y el pseudocódigo para representarlo, utilizando el ciclo apropiado.
+
+
